eeff934ef3b59ad1a22c5b7f69f145bafdacf8a1
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2021-10-17 16:52:00 -0300

    Adicionando TextView

--- app/src/main/java/br/com/erick/erickviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/br/com/erick/erickviagens/ui/activity/ListaPacotesActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import br.com.erick.erickviagens.R;
+
 public class ListaPacotesActivity extends AppCompatActivity {
 
     @Override
