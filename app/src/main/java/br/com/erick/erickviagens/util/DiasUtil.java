8fa185028cc58c3cd6d5bd590d0b27e357b0c210
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2021-10-17 23:13:40 -0300

    Refatoracao da classe ListaPacotesAdapter e criacao das classes ResourceUtil, DiasUtil, MoedaUtil dentro do pacote 'util'

---
+++ app/src/main/java/br/com/erick/erickviagens/util/DiasUtil.java
@@ -0,0 +1,20 @@
+package br.com.erick.erickviagens.util;
+
+import androidx.annotation.NonNull;
+
+import br.com.erick.erickviagens.model.Pacote;
+
+public class DiasUtil {
+
+    public static final String PLURAL = " dias";
+    public static final String SINGULAR = " dia";
+
+    @NonNull
+    public static String formataEmTexto(int quantidadeDeDias) {
+        if (quantidadeDeDias > 1){
+            return quantidadeDeDias + PLURAL;
+        }
+        return quantidadeDeDias + SINGULAR;
+
+    }
+}
