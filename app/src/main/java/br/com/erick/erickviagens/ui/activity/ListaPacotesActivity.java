5fcd95053fd843c1c38cd02a52b80c149b9b985d
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2021-10-17 22:17:46 -0300

    Adapter Criado

--- app/src/main/java/br/com/erick/erickviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/br/com/erick/erickviagens/ui/activity/ListaPacotesActivity.java
@@ -3,8 +3,15 @@ package br.com.erick.erickviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
 
 import br.com.erick.erickviagens.R;
+import br.com.erick.erickviagens.dao.PacoteDAO;
+import br.com.erick.erickviagens.model.Pacote;
+import br.com.erick.erickviagens.ui.adapter.ListaPacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -12,5 +19,11 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
 }
