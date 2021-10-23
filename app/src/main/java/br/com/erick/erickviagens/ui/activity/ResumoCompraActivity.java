40b27caa39882ecff117060a693f7dea08cb09c6
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2021-10-23 01:34:37 -0300

    Fluxo entre as telas adicionado

--- app/src/main/java/br/com/erick/erickviagens/ui/activity/ResumoCompraActivity.java
+++ app/src/main/java/br/com/erick/erickviagens/ui/activity/ResumoCompraActivity.java
@@ -2,8 +2,11 @@ package br.com.erick.erickviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
