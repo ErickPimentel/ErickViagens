c88b8c191cb6305bf59987195a414a44d8155668
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2021-10-23 00:16:52 -0300

    funcao mostrarPreco em PagamentoActivity adicionada

--- app/src/main/java/br/com/erick/erickviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/br/com/erick/erickviagens/ui/activity/PagamentoActivity.java
@@ -3,14 +3,34 @@ package br.com.erick.erickviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import br.com.erick.erickviagens.R;
+import br.com.erick.erickviagens.model.Pacote;
+import br.com.erick.erickviagens.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        mostraPreco(pacoteSaoPaulo);
+
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco_pacote);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
